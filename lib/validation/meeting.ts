481dd02aab11dbeba7b668b052fa777260606100
Deihl_Reyes <78899978+DeihlReyes@users.noreply.github.com>
2024-01-13 01:02:49 +0800

    fix: change zod type of date in meeting schema

--- lib/validation/meeting.ts
+++ lib/validation/meeting.ts
@@ -4,7 +4,7 @@ export const meetingSchema = z.object({
   title: z.string().min(1, { message: "Title is Required" }).max(100, { message: "Title is Too Long" }),
   description: z.string().min(1, { message: "Description is Required" }).max(150, { message: "Description is Too Long" }),
   link: z.string().url({ message: "Link is Required" }),
-  date: z.date({
+  date: z.coerce.date({
     required_error: "A due date is required.",
   }),
 });
