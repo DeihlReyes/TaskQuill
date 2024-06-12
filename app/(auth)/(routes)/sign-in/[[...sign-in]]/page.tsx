19a24d38c4676d6c40eb3557fe50ad98276cc37f
Deihl_Reyes <78899978+DeihlReyes@users.noreply.github.com>
2024-06-12 21:38:48 +0800

    style: change width of demo acc card

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -2,8 +2,8 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="relative flex h-screen justify-center items-center">
-      <div className="bg-primary/10 w-full py-6 px-12 mt-10 absolute top-20 rounded-xl">
+    <div className="relative flex h-screen justify-center items-center w-full max-w-[400px]">
+      <div className="bg-primary/10 w-full py-6 px-12 mt-10 absolute top-10 md:top-20 rounded-xl">
           <h2 className="text-base mb-2">Demo Account: </h2>
           <p className="text-sm">Email: demo+clerk_test@gmail.com</p>
           <p className="text-sm">Password: thisisdemo123</p>
